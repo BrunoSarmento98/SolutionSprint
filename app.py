2567df622dc1f79c068f4dd02d306e30abc2d13a
BrunoSarmento98 <122676180+BrunoSarmento98@users.noreply.github.com>
2023-04-23 09:01:37 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Esse é o trabalho final do Grupo 28 Versão Final"
+    return "Esse é o trabalho final do Grupo 28 Versão Final Vídeo"
 
 if __name__ == '__main__':
     app.run()
